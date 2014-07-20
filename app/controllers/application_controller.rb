97b27f33a166e2e04dae45550c47720a57ef5217
brijeshgpt7 <brijeshgpt7@gmail.com>
2014-07-20 14:51:23 +0530

    conflict push

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  render :text=> "okokokok"
-  protect_from_forgery with: :exception
+
+
+render :text="okokoko"  
+protect_from_forgery with: :exception
 end
