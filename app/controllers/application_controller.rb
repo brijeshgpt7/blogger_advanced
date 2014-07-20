c30b9931bde14f3b67ea89c62816f8102636b949
brijeshgpt7 <brijeshgpt7@gmail.com>
2014-07-20 17:27:46 +0530

    hi after merge

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,6 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-
-
-render :text="okokoko"  
-protect_from_forgery with: :exception
+   render :text="okokoko"  
+   protect_from_forgery with: :exception
 end
