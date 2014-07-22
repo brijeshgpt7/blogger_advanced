dbb17aef5fa40cb2434c1c0113294aee31d2d187
brijeshgpt7 <brijeshgpt7@gmail.com>
2014-07-22 22:29:58 +0530

    This is the commit that i want to change

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,13 @@ class ApplicationController < ActionController::Base
    render :text="okokoko"  
    protect_from_forgery with: :exception
    redirect_to users_path
+
+################################## This is first commit    #############################################
+ For APIs, you may want to use :null_session instead.
+   render :text="okokoko"
+   protect_from_forgery with: :exception
+   redirect_to users_path
+
+
+####################################################################################
 end
